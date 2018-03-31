c4cc848e5faf8840435f002a825a5fbb0c4ce096
Elena Medina <ElenaMedina@quickenloans.com>
2018-03-30 22:38:55 -0400

    Initial base files for this project. Nothing to see here...

---
+++ GameDayPlanner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPlanner
+{
+    class GameDayPlanner
+    {
+        static void Main(string[] args)
+        {
+            Person Rich = new Person("Rich", 30, "brown", 1.5f);           
+            Rich.PrintPerson();
+            Console.ReadKey();
+
+            Person Elena = new Person("Elena", 33, "brown");
+            Elena.PrintPerson();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPlanner
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public string eyeColor;
+        public float beardLength;
+        public Person(string Name, int Age, string EyeColor = "brown", float BeardLength = 0)
+        {
+            name = Name;
+            age = Age;
+            eyeColor = EyeColor;
+            beardLength = BeardLength;
+        }
+
+        public void PrintPerson()
+        { 
+            Console.WriteLine("My name is " + name + ".");
+            Console.WriteLine("I am " + age + " years old.");
+            Console.WriteLine("I have " + eyeColor + " eyes.");
+            if (beardLength == 0)
+            {
+                Console.WriteLine("I do not have a beard.");
+            }
+            else
+            {
+                Console.WriteLine("My beard is " + beardLength + " inch(es) long.");
+            }
+            
+        }
+    }
+}
