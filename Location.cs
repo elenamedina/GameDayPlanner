fe334739655b04b037fc91686659cb1b13952a0b
Elena Medina <ElenaMedina@quickenloans.com>
2018-04-08 21:00:09 -0400

    Build out location class
    added eateries arrays to each location in switch statement. added a randomizer method within the class to randomly select an eatery and output the selection.

--- Location.cs
+++ Location.cs
@@ -16,27 +16,35 @@ namespace PartyPlanner
                 case 1:
                     {
                         Console.WriteLine("You will be playing in Grosse Pointe Park!");
+                        string[] eateries = {"Red Crown", "Atwater", "Cadieux Cafe", "Marais" };
+                        restaurantRandomizer(eateries);
                         Console.ReadKey();
                         break;
                     }
                 case 2:
                     {
                         Console.WriteLine("You will be playing in Royal Oak!");
+                        string[] eateries = { "Lockharte's BBQ", "Hopcat", "Jolly Pumpkin", "Royal Oak Brewery" };
+                        restaurantRandomizer(eateries);
                         Console.ReadKey();
                         break;
                     }
                 case 3:
                     {
                         Console.WriteLine("You will be playing in Detroit!");
+                        string[] eateries = { "Central", "Sy Thai", "Hopcat", "Grand Trunk"};
+                        restaurantRandomizer(eateries);
                         Console.ReadKey();
                         break;
                     }
-            }           
+            }
         }
 
-        //public void PrintLocation()
-        //{
-            
-        //}
+        private void restaurantRandomizer(string[] eateries)
+        {
+            Random rnd = new Random();
+            int eIndex = rnd.Next(eateries.Length);
+            Console.WriteLine("Suggested noms for the big day: {0}", eateries[eIndex]);
+        }
     }
 }
